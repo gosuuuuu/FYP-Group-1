87ae6f24daca0977a9a930524f7f243c57d57efb
AinaRezal <ainarezal.27@gmail.com>
2024-08-14 23:23:57 +0800

    Update #3
    Aina - Designing Streamlit and discussion

--- fyp_st.py
+++ fyp_st.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 from streamlit_option_menu import option_menu
+import base64
 
 st.set_page_config(layout="wide",
                    initial_sidebar_state="collapsed",
@@ -13,29 +14,44 @@ st.set_page_config(layout="wide",
 st.markdown("<h1 style='text-align: center; color: black; background-color: #cfffdd'>I D E N T I L O G</h1>", 
             unsafe_allow_html=True)
 
-
-
-col = st.columns(2)
-with col[0]:
-    st.write('Insert something')
-
-with col[1]:
-    menu = option_menu(None, ["home", "upload"], 
-        icons=['house', 'cloud-upload'],
-        menu_icon="cast", default_index=0, orientation="horizontal",
-        styles={
-            "container": {"padding": "0!important", 
-                          "background-color": "#cfffdd"},
-            "icon": {"color": "green", 
-                     "font-size": "20px"}, 
-            "nav-link": {"font-size": "14px",
-                         "font-family": "arial" , # to be changed
-                         "text-align": "left", 
-                         "margin":"0px", 
-                         "--hover-color": "#eee"},
-            "nav-link-selected": {"background-color": "#19c24a"}
-        }
+def set_bg(img_path):
+    bg_ext = 'png'
+    
+    with open(img_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode()
+    
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background: url(data:image/{bg_ext};base64,{encoded_string});
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
     )
+
+img_bg = r"C:\Users\User\OneDrive\Desktop\nyum.png"
+set_bg(img_bg)
+
+
+menu = option_menu(None, ["home", "upload"], 
+    icons=['house', 'cloud-upload'],
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", 
+                        "background-color": "#cfffdd"},
+        "icon": {"color": "green", 
+                    "font-size": "20px"}, 
+        "nav-link": {"font-size": "14px",
+                        "font-family": "arial" , # to be changed
+                        "text-align": "center", 
+                        "margin":"0px", 
+                        "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#19c24a"}
+    }
+)
 
 if menu == 'home':
 
@@ -52,18 +68,35 @@ if menu == 'upload':
     classes=["tidyman", "plastic_PS", "plastic_PP",
             "plastic_PET", "plastic_PAP", "plastic_other",
             "plastic_LDPE", "plastic_HDPE", "period_36m",
-            "period_24m", "plastic_12m", "period_6m",
-            "period_3m", "mobius_logo", "fsc",
-            "ce_marking", "aluminium"]
+            "period_24m", "period_12m", "period_6m",
+            "period_3m", "period_9m", "mobius_logo", "fsc",
+            "ce_marking", "aluminium"] # 18 classes
 
-    uploader = st.camera_input('test')
+    st.write('pog')
+    menu_2 = option_menu(None, ["one", "multiple"], 
+    icons=['house', 'cloud-upload'],
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", 
+                        "background-color": "#cfffdd"},
+        "icon": {"color": "green", 
+                    "font-size": "20px"}, 
+        "nav-link": {"font-size": "14px",
+                        "font-family": "arial" , # to be changed
+                        "text-align": "center", 
+                        "margin":"0px", 
+                        "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#19c24a"}
+    }
+)
+    upload_button = st.file_uploader('upload image', type='jpg')
     # Creating functions:
     def load_model():
         model = tf.keras.models.load_model('D:\Data Related Stuffs\FYP Model\FYP-Group-1\ResNet50.h5')
         return model
 
-    def load_img (img):
-        img = Image.open(uploader)
+    def load_img (uploaded_file):
+        img = Image.open(uploaded_file)
         img = img.resize((224,224))
         img_array = np.array(img)
         img_tensor = tf.convert_to_tensor(img_array, dtype=tf.float32)
@@ -73,9 +106,10 @@ if menu == 'upload':
         return img_tensor
 
     # Main function
-    if uploader:
+        
+    if upload_button:
         model = load_model()
-        tensor = load_img(uploader)
+        tensor = load_img(upload_button)
         prediction = model.predict(tensor)
         pred_class = classes[np.argmax(prediction)]
         st.write("predicted class:", pred_class)
