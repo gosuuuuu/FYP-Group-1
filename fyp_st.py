69b3a399ea05e402a22d50b3210e7274eb3c9158
gosuuuuu <imsyabil@gmail.com>
2024-08-27 18:05:01 +0800

    Update #17

--- fyp_st.py
+++ fyp_st.py
@@ -17,7 +17,7 @@ def black_font():
     st.markdown(
         """ <style>
         * {color: black !important;
-            font-size: 18px !important;
+            font-size: 21px !important;
         }
         </style>
         """,
@@ -65,7 +65,7 @@ menu = option_menu(None, ["Home", "Upload Logo Prediction"],
                         "background-color": "#cfffdd"},
         "icon": {"color": "green", 
                     "font-size": "20px"}, 
-        "nav-link": {"font-size": "14px",
+        "nav-link": {"font-size": "20px",
                         "font-family": "arial" , # to be changed
                         "text-align": "center", 
                         "margin":"0px", 
@@ -75,7 +75,7 @@ menu = option_menu(None, ["Home", "Upload Logo Prediction"],
 )
 
 # Home Page
-if menu == 'home':
+if menu == 'Home':
     tab1, tab2, tab3 = st.tabs(['Description', 'Recycle Logos', 'Contact'])
 
     with tab1: # Description Tab
@@ -88,216 +88,235 @@ if menu == 'home':
         st.title("This is the Recycle Logos tab \n")
         st.write("Few logos that included in the project")
 
-        # Set Container Configuration
-        st.markdown(
+        col1, col2, col3, col4 = st.columns([3,6,3,6])
+
+        with col1:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/aluminium.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
+
+        with col2:
+            st.subheader("Aluminium")
+            st.markdown(
             """
-            <style>
-            .col-container {
-                display: flex;
-                align-items: center;
-            }
-            .col-container .col {
-                margin-right: 1000px;  /* Adjust this value to increase or decrease spacing between columns */
-            }
-            .col-container .col:last-child {
-                margin-right: 100px;  /* No margin for the last column */
-            }
-            </style>
+            <p style='font-size:100px;'>
+            The Recyclable Aluminium Recyclable Aluminium Indicates that the item is made from aluminum, a highly recyclable material.
+            </p>
             """,
-            unsafe_allow_html=True
-        )
+            unsafe_allow_html=True,
+            )
 
-        with st.container():
-            st.markdown('<div class="col-container">', unsafe_allow_html=True)
+        with col3:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/ce.png" 
+                        alt="Aluminium Logo" 
+                        width="250">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            col1, col2, col3, col4 = st.columns([3,4,3,4])
+        with col4:
+            st.subheader("CE")
+            st.write(
+                """
+                This indicates conformity with health, safety, and environmental protection standards for products sold within the European Economic Area.  
+                """
+            )
 
-            with col1:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/aluminium.png",
-                    caption="Aluminium Logo",
-                    use_column_width=False,
-                    width=200,           
-                )
+        col5, col6, col7, col8 = st.columns([3,6,3,6])
 
-            with col2:
-                st.subheader("Aluminium")
-                st.write(
-                    """
-                    The Recyclable Aluminium Recyclable Aluminium Indicates that the item is made from aluminum, a highly recyclable material.  
-                    """
-                )
+        with col5:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/fsc.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            with col3:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/ce.png",
-                    caption="CE logo",
-                    use_column_width=False,
-                    width=250,  
-                )
-
-            with col4:
-                st.subheader("CE")
-                st.write(
-                    """
-                    This indicates conformity with health, safety, and environmental protection standards for products sold within the European Economic Area.  
-                    """
-                )
+        with col6:
+            st.subheader("FSC")
+            st.write(
+                """
+                This certifies that the wood or paper products come from responsibly managed forests.  
+                """
+            )
             
-            st.markdown('</div>', unsafe_allow_html=True)
+        with col7:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/mobiusloop.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            col5, col6, col7, col8 = st.columns(4)
+        with col8:
+            st.subheader("Mobius Loop")
+            st.write(
+                """
+                This represents that the product is recyclable.  
+                """
+            )
 
-            with col5:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/fsc.png",
-                    caption="FSC Logo",
-                    use_column_width=False,
-                    width=200,  
-                )
+        col9, col10, col11, col12 = st.columns([3,6,3,6])
 
-            with col6:
-                st.subheader("FSC")
-                st.write(
-                    """
-                    This certifies that the wood or paper products come from responsibly managed forests.  
-                    """
-                )
-            
-            with col7:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/mobiusloop.png",
-                    caption="Mobius Loop Logo",
-                    use_column_width=False,
-                    width=200,  
-                )
+        with col9:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/pao.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            with col8:
-                st.subheader("Mobius Loop")
-                st.write(
-                    """
-                    This represents that the product is recyclable.  
-                    """
-                )
+        with col10:
+            st.subheader("Period After Opening")
+            st.write(
+                """
+                The period-after-opening (PAO) symbol highlights the amount of time a cosmetic product will remain safe for use after being opened for the first time. It depicts an open cosmetics pot and is used together with a written number of months or years.  
+                """
+            )
 
-            col9, col10, col11, col12 = st.columns(4)
+        with col11:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/plastic.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            with col9:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/pao.png",
-                    caption="Period After Opening Logo",
-                    use_column_width=False,
-                    width=200,  
-                )
+        with col12:
+            st.subheader("Plastic")
+            st.write(
+                """
+                These symbols help consumers to identify different types of plastic resin used to make the product.They were applied to help assist with the sorting of plastics. Like many materials plastics should be recycled separately, so the value of the of the recycled material is preserved. Allowing it to be reused to make new products.  
+                """
+            )
 
-            with col10:
-                st.subheader("Period After Opening")
-                st.write(
-                    """
-                    The period-after-opening (PAO) symbol highlights the amount of time a cosmetic product will remain safe for use after being opened for the first time. It depicts an open cosmetics pot and is used together with a written number of months or years.  
-                    """
-                )
+        col13, col14, col15, col16 = st.columns([3,6,3,6])
 
-            with col11:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/plastic.png",
-                    caption="Plastic Logo",
-                    use_column_width=False,
-                    width=200,  
-                )
+        with col13:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/tidyman.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            with col12:
-                st.subheader("Plastic")
-                st.write(
-                    """
-                    These symbols help consumers to identify different types of plastic resin used to make the product.They were applied to help assist with the sorting of plastics. Like many materials plastics should be recycled separately, so the value of the of the recycled material is preserved. Allowing it to be reused to make new products.  
-                    """
-                )
+        with col14:
+            st.subheader("Tidyman")
+            st.write(
+                """
+                A symbol encouraging proper waste disposal.  
+                """
+            )
 
-            col13, col14, col15, col16 = st.columns(4)
+        with col15:
+            st.markdown(
+                """
+                <div style='text-align: center;'>
+                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/waste.png" 
+                        alt="Aluminium Logo" 
+                        width="200">
+                    <p>Aluminium Logo</p>
+                </div>
+                """,
+                unsafe_allow_html=True,
+            )
 
-            with col13:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/tidyman.png",
-                    caption="Tidyman Logo",
-                    use_column_width=False,
-                    width=200,  
-                )
+        with col16:
+            st.subheader("Electric Waste and Battery")
+            st.write(
+                """
+                he crossed-out wheelie bin symbol on your electrical product, batteries, or their packaging, reminds you that all electrical and electronic products and batteries must be recycled through either a recycling centre or with retailers. 
+                """
+            )
 
-            with col14:
-                st.subheader("Tidyman")
-                st.write(
-                    """
-                    A symbol encouraging proper waste disposal.  
-                    """
-                )
+    # with tab3: # Contact tab
 
-            with col15:
-                st.image(
-                    "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/waste.png",
-                    caption="Electric Waste and Battery Logo",
-                    use_column_width=False,
-                    width=200,  
-                )
+    #     st.title(":envelope: Get In Touch With Us!")
 
-            with col16:
-                st.subheader("Electric Waste and Battery")
-                st.write(
-                    """
-                    he crossed-out wheelie bin symbol on your electrical product, batteries, or their packaging, reminds you that all electrical and electronic products and batteries must be recycled through either a recycling centre or with retailers. 
-                    """
-                )
-
-    with tab3: # Contact tab
-
-        st.title(":envelope: Get In Touch With Us!")
-
-        contact_form = """
-            <style>
-            form {
-                width: 100%;
-            }
-            input[type=text], input[type=email], textarea {
-                width: 100%;
-                padding: 12px;
-                border: 1px solid #ccc;
-                border-radius: 4px;
-                box-sizing: border-box;
-                margin-top: 6px;
-                margin-bottom: 16px;
-                background-color: #FFFFFF;
-            }
-            textarea {
-                resize: vertical;
-            }
-            button[type=submit] {
-                background-color: #04AA6D;
-                color: white;
-                padding: 12px 20px;
-                border: none;
-                border-radius: 4px;
-                cursor: pointer;
-            }
-            button[type=submit]:hover {
-                background-color: #45a049;
-            }
-            </style>
-            <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
-                <input type="hidden" name="_captcha" value="false">
-                <input type="text" name="name" placeholder="Your name" required>
-                <input type="email" name="email" placeholder="Your email" required>
-                <textarea name="message" placeholder="Your message here"></textarea>
-                <button type="submit">Send</button>
-            </form>
-        """
-        st.markdown(contact_form, unsafe_allow_html=True)
+    #     contact_form = """
+    #         <style>
+    #         form {
+    #             width: 100%;
+    #         }
+    #         input[type=text], input[type=email], textarea {
+    #             width: 100%;
+    #             padding: 12px;
+    #             border: 1px solid #ccc;
+    #             border-radius: 4px;
+    #             box-sizing: border-box;
+    #             margin-top: 6px;
+    #             margin-bottom: 16px;
+    #             background-color: #FFFFFF;
+    #         }
+    #         textarea {
+    #             resize: vertical;
+    #         }
+    #         button[type=submit] {
+    #             background-color: #04AA6D;
+    #             color: white;
+    #             padding: 12px 20px;
+    #             border: none;
+    #             border-radius: 4px;
+    #             cursor: pointer;
+    #         }
+    #         button[type=submit]:hover {
+    #             background-color: #45a049;
+    #         }
+    #         </style>
+    #         <form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+    #             <input type="hidden" name="_captcha" value="false">
+    #             <input type="text" name="name" placeholder="Your name" required>
+    #             <input type="email" name="email" placeholder="Your email" required>
+    #             <textarea name="message" placeholder="Your message here"></textarea>
+    #             <button type="submit">Send</button>
+    #         </form>
+    #     """
+    #     st.markdown(contact_form, unsafe_allow_html=True)
 
 # insert description of website
 # how to use upload function steps
 # table of contents with links
 
 # Upload Page
-if menu == 'upload':
+if menu == 'Upload Logo Prediction':
     # 18 classes of logo
     classes=["tidyman", "plastic_PS", "plastic_PP",
             "plastic_PET", "plastic_PAP", "plastic_other",
@@ -306,8 +325,9 @@ if menu == 'upload':
             "period_3m", "period_9m", "mobius_logo", "fsc",
             "ce_marking", "aluminium"]
 
-    st.write('Insert description or text here') # To insert description
-    
+    st.header('Do you want to know how to dispose of your rubish? We can help you with that.\n') # To insert description
+    st.write('Find any recycle logo on your items and upload it!\n')
+
 # Modal
     # Creating Modal
     modal_number_logos = Modal("Number Of Logos And File",
@@ -333,11 +353,12 @@ if menu == 'upload':
 
     if modal_number_logos.is_open(): # When the variable is opened
         with modal_number_logos.container():
-            st.session_state.number_logos = st.number_input('Insert',
+            st.session_state.number_logos = st.number_input('Insert how many recycle logos you want to know',
                                            min_value=1,
                                            step=1)
-            st.session_state.uploaded_img = st.file_uploader('Insert File here')
-            done_button = st.button('Done!',
+            st.session_state.uploaded_img = st.file_uploader('Insert File here into "Browse files"')
+            st.write('Click "Done insert" if you are done inserting')
+            done_button = st.button('Done insert!',
                                use_container_width=True)
         if done_button:
             modal_number_logos.close() # Modal page is closed    
