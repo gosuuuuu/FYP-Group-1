6e88883fb06d7c66329c3817b6eb1bab0afb26e8
AinaRezal <ainarezal.27@gmail.com>
2024-10-04 00:12:08 +0800

    Update #37
    Weird

--- fyp_st.py
+++ fyp_st.py
@@ -27,6 +27,8 @@ def black_font():
     )
 black_font()
 
+st.markdown('<div class="container">', unsafe_allow_html=True)
+
 # Set Logo using URL
 def set_logo(logo_url):
     st.markdown(
@@ -59,47 +61,63 @@ img_url = "https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Logo%20an
 set_bg(img_url)
 
 
-#################### OPTION MENU ####################
-menu = option_menu(None, ["Home", "Upload Logo Prediction"], 
-    icons=['house', 'cloud-upload'],
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", 
-                        "background-color": "#cfffdd"},
-        "icon": {"color": "green", 
-                    "font-size": "20px"}, 
-        "nav-link": {"font-size": "20px",
-                        "font-family": "arial" , # to be changed
-                        "text-align": "center", 
-                        "margin":"0px", 
-                        "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#19c24a"}
+st.markdown("""<style>
+    body {
+        margin: 0;
+        padding: 0;
     }
-)
-
-
-#################### OPTION MENU ####################
-menu = option_menu(None, ["Home", "Upload Logo Prediction"], 
-    icons=['house', 'cloud-upload'],
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", 
-                        "background-color": "#cfffdd"},
-        "icon": {"color": "green", 
-                    "font-size": "20px"}, 
-        "nav-link": {"font-size": "20px",
-                        "font-family": "arial" , # to be changed
-                        "text-align": "center", 
-                        "margin":"0px", 
-                        "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#19c24a"}
+    .top-bar {
+        background-color: #80ed99;
+        padding: 0;
+        margin: 0;
+        position: fixed;
+        top: 80px;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 70px;
+        z-index: 1000;
+        border-bottom: 2px solid #e0e0e0;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
     }
-)
+    .top-bar a {
+        padding: 15px;
+        text-decoration: none;
+        color: #000;
+        font-weight: bold;
+        flex: 1;
+        text-align: center;
+    }
+    .top-bar a:hover {
+        color: #DAD7CD;
+    }
+    .container {
+        margin-top: 110px;
+        padding: 20px;
+    }
+    .logo {
+        text-align: center;
+        margin-top: 0px;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
+st.markdown("""<div class="top-bar">
+        <a href="?page=home">Home</a>
+        <a href="?page=upload-prediction">Upload logo prediction</a>
+        <a href="?page=description">Description</a>
+        <a href="?page=recycle-logos">Recycle Logos</a>
+    </div>
+""", unsafe_allow_html=True)
 
+# Get the query parameters
+query_params = st.query_params
+page = query_params.get("page", "home")
 
 # Home Page
-if menu == 'Home':
+if page == 'Home':
     st.markdown(
                 """
                 <div style='text-align: center;'>
@@ -110,168 +128,166 @@ if menu == 'Home':
                 unsafe_allow_html=True,
             )
 
-    tab1, tab2 = st.tabs(['Description', 'Recycle Logos'])
-
-elif page == "description":
+elif page == "upload-prediction":
     st.title("This is the Description tab. \n")
     st.write("Recycling symbols are used to help us identify different types of packaging and if they are capable of being recycled.  \n"
         "They can be confusing, so we are here to help you make sense of them and hopefully increase what you recycle in and out of the home.")
-    
+        
 elif page == "recycle-logos":
     st.title("This is the Recycle Logos tab \n")
     st.write("Few logos that included in the project")
 
-        col1, col2, col3, col4 = st.columns([3,6,3,6])
+    col1, col2, col3, col4 = st.columns([3,6,3,6])
 
-        with col1:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/aluminium.png" 
-                        alt="Aluminium Logo" 
-                        width="180">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
-
-        with col2:
-            st.subheader("Aluminium")
-            st.markdown(
+    with col1:
+        st.markdown(
             """
-            <p style='font-size:100px;'>
-            ALU: Represents aluminum, which can be recycled at The Green Depot in Kuala Belait but typically requires specialized processing. Recycling aluminum helps reduce energy consumption and environmental impact. For more information, visit The Green Depot.
-            </p>
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/aluminium.png" 
+                    alt="Aluminium Logo" 
+                    width="180">
+            </div>
             """,
             unsafe_allow_html=True,
-            )
-            st.link_button("Visit The Green Depot", "https://www.instagram.com/greendepotbn")
+        )
 
-        with col3:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/ce.png"  
-                        width="230">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+    with col2:
+        st.subheader("Aluminium")
+        st.markdown(
+        """
+        <p style='font-size:100px;'>
+        ALU: Represents aluminum, which can be recycled at The Green Depot in Kuala Belait but typically requires specialized processing. Recycling aluminum helps reduce energy consumption and environmental impact. For more information, visit The Green Depot.
+        </p>
+        """,
+        unsafe_allow_html=True,
+        )
+        st.link_button("Visit The Green Depot", "https://www.instagram.com/greendepotbn")
 
-        with col4:
-            st.subheader("CE")
-            st.write(
-                """
-                This indicates conformity with health, safety, and environmental protection standards for products sold within the European Economic Area.  
-                """
-            )
+    with col3:
+        st.markdown(
+            """
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/ce.png"  
+                    width="230">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
-        col5, col6, col7, col8 = st.columns([3,6,3,6])
+    with col4:
+        st.subheader("CE")
+        st.write(
+            """
+            This indicates conformity with health, safety, and environmental protection standards for products sold within the European Economic Area.  
+            """
+        )
 
-        with col5:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/fsc.png" 
-                        width="170">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+    col5, col6, col7, col8 = st.columns([3,6,3,6])
 
-        with col6:
-            st.subheader("FSC")
-            st.write(
-                """
-                This certifies that the wood or paper products come from responsibly managed forests. it can be recycled at The Green Depot.   
-                """
-            )
-            st.link_button("Visit The Green Depot", "https://www.instagram.com/greendepotbn")
-            
-        with col7:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/mobiusloop.png" 
-                        width="180">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+    with col5:
+        st.markdown(
+            """
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/fsc.png" 
+                    width="170">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
-        with col8:
-            st.subheader("Mobius Loop")
-            st.write(
-                """
-                This symbol indicates recyclability, though whether a product can be recycled easily depends on local facilities. In Brunei, it may require specialized facilities like The Green Depot.   
-                """
-            )
-            st.link_button("Visit The Green Depot", "https://www.instagram.com/greendepotbn")
+    with col6:
+        st.subheader("FSC")
+        st.write(
+            """
+            This certifies that the wood or paper products come from responsibly managed forests. it can be recycled at The Green Depot.   
+            """
+        )
+        st.link_button("Visit The Green Depot", "https://www.instagram.com/greendepotbn")
+        
+    with col7:
+        st.markdown(
+            """
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/mobiusloop.png" 
+                    width="180">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
-        col9, col10, col11, col12 = st.columns([3,6,3,6])
+    with col8:
+        st.subheader("Mobius Loop")
+        st.write(
+            """
+            This symbol indicates recyclability, though whether a product can be recycled easily depends on local facilities. In Brunei, it may require specialized facilities like The Green Depot.   
+            """
+        )
+        st.link_button("Visit The Green Depot", "https://www.instagram.com/greendepotbn")
 
-        with col9:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/pao.png"  
-                        width="200">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+    col9, col10, col11, col12 = st.columns([3,6,3,6])
 
-        with col10:
-            st.subheader("Period After Opening")
-            st.write(
-                """
-                The period-after-opening (PAO) symbol highlights the amount of time a cosmetic product will remain safe for use after being opened for the first time. It depicts an open cosmetics pot and is used together with a written number of months or years.  
-                """
-            )
+    with col9:
+        st.markdown(
+            """
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/pao.png"  
+                    width="200">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
-        with col11:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/plastic.png"  
-                        width="200">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+    with col10:
+        st.subheader("Period After Opening")
+        st.write(
+            """
+            The period-after-opening (PAO) symbol highlights the amount of time a cosmetic product will remain safe for use after being opened for the first time. It depicts an open cosmetics pot and is used together with a written number of months or years.  
+            """
+        )
 
-        with col12:
-            st.subheader("Plastic")
-            st.write(
-                """
-                These symbols help consumers to identify different types of plastic resin used to make the product.They were applied to help assist with the sorting of plastics. Like many materials plastics should be recycled separately, so the value of the of the recycled material is preserved. Allowing it to be reused to make new products.  
-                """
-            )
+    with col11:
+        st.markdown(
+            """
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/plastic.png"  
+                    width="200">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
 
-        col13, col14, col15, col16 = st.columns([3,6,3,6])
+    with col12:
+        st.subheader("Plastic")
+        st.write(
+            """
+            These symbols help consumers to identify different types of plastic resin used to make the product.They were applied to help assist with the sorting of plastics. Like many materials plastics should be recycled separately, so the value of the of the recycled material is preserved. Allowing it to be reused to make new products.  
+            """
+        )
 
-        with col13:
-            st.markdown(
-                """
-                <div style='text-align: center; margin: 0px 100px 50px 0px;'>
-                    <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/tidyman.png" 
-                        width="180">
-                </div>
-                """,
-                unsafe_allow_html=True,
-            )
+    col13, col14, col15, col16 = st.columns([3,6,3,6])
 
-        with col14:
-            st.subheader("Tidyman")
-            st.write(
-                """
-                 This symbol is a reminder to dispose of waste responsibly. It encourages consumers to throw their trash into appropriate trash cans, helping to keep the environment clean. While not a recycling logo, it promotes good waste management practices.  
-                """
-            )
+    with col13:
+        st.markdown(
+            """
+            <div style='text-align: center; margin: 0px 100px 50px 0px;'>
+                <img src="https://raw.githubusercontent.com/gosuuuuu/FYP-Group-1/main/Recycle%20Logo/tidyman.png" 
+                    width="180">
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+
+    with col14:
+        st.subheader("Tidyman")
+        st.write(
+            """
+                This symbol is a reminder to dispose of waste responsibly. It encourages consumers to throw their trash into appropriate trash cans, helping to keep the environment clean. While not a recycling logo, it promotes good waste management practices.  
+            """
+        )
 
 
 # Upload Page
-if menu == 'Upload Logo Prediction':
+elif page == "upload-prediction":
     my_prediction = LogoClassfier("G:\My Drive\Poli\SEM 5\ResNet50.h5") # Change to google drive
     
     # 18 classes of logo
@@ -367,9 +383,3 @@ if menu == 'Upload Logo Prediction':
         
 else:
     st.write('Please upload an image.')
-
-menu2 = option_menu(None, ["Recycle", "Upload Logo Prediction"],)
-if menu2 == 'Recycle':
-    st.markdown(
-        st.title("Recyle"))
-                
